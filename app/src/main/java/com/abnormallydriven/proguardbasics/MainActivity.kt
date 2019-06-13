799c4fe5f02dd85af6a4dfa3cd66fbb4cca653e9
Brett <bretttucker@gmail.com>
2019-06-13 13:31:04 -0700

    Add reflection example

--- app/src/main/java/com/abnormallydriven/proguardbasics/MainActivity.kt
+++ app/src/main/java/com/abnormallydriven/proguardbasics/MainActivity.kt
@@ -2,11 +2,19 @@ package com.abnormallydriven.proguardbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clazz = Class.forName("com.abnormallydriven.proguardbasics.Message")
+        val constructor = clazz.getConstructor(String::class.java)
+
+        val myReflectedMessage = constructor.newInstance("My Message String")
+        Log.d("ProguardBasics", myReflectedMessage.toString())
+
     }
 }
